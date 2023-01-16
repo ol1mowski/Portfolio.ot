3fd3726ee0138de48c8ab81c42bce638fda757d0
Oliwier Markiewicz <101707516+ol1mowski@users.noreply.github.com>
2023-01-16 13:26:21 +0100

    Portfolio_version_1
    ptv1

---
+++ app.js
@@ -0,0 +1,27 @@
+function scroll(e) {
+    const href = $(this).attr('href');
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, 800);
+
+    location.hash = href;
+};
+
+$('a[href*="#"]').click(scroll);
+
+
+//Sticky MENU
+window.onscroll = function() { myFunction() };
+
+const navbar = document.getElementById("header");
+
+const sticky = navbar.offsetTop;
+
+let myFunction = () => {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
